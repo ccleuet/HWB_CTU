f82920e2854e5205adaaf27bba0579413f0a7dea
Your Name <you@example.com>
2018-03-31 14:38:35 +0200

    Add Lab 4

--- Lab2/DPA.m
+++ Lab2/DPA.m
@@ -19,6 +19,7 @@ tm     = mean(traces, 2);
 traces = traces - tm(:,ones(1,size(traces,2))) + mm;
 toc
 
+plot (traces);
 disp('load text')
 inputs = plaintextInput('plaintext.txt', plaintextLength, numOfTraces);
 
@@ -30,31 +31,50 @@ disp('**** Add your code to complete the analysis here ****')
 keyHypo = (0:255);
 keyMat = repmat(keyHypo,numOfTraces,1);
 key_final=zeros(16,1);
+correct_key=[202, 254, 186, 190, 163, 88, 119, 204, 17, 0, 186, 34, 107, 111, 107, 111];
 
 for i = 1:16
     
 ByteVector=inputs(:,i)';
 ByteMat = repmat(ByteVector,256,1)';
 CipherByteHypo=bitxor(keyMat,ByteMat);
+after_sbox=SubBytes(CipherByteHypo + 1);
 
-PowerConsumption = byte_Hamming_weight(SubBytes(CipherByteHypo + 1) + 1);
+% Hamming Weight
+PowerConsumption_HW= byte_Hamming_weight( after_sbox + 1);
 
+% Hamming Distance
+
+PowerConsumption_HD= bitxor(1, after_sbox +1 );
+
+% Single bit
+ PowerConsumption = bitget(after_sbox,5);
+     
 %Pearson correlation
 
 ByteCorMat = myCorrcoef(PowerConsumption, traces);
 highestCorCoef = max(ByteCorMat(:));
 [key, time] = find(ismember(ByteCorMat, highestCorCoef));
-    if i==1
-        x=1:38000;
-        right_guess=ByteCorMat(key,1:38000);
-        wrong_guess_1=ByteCorMat(1,1:38000);
-        wrong_guess_2=ByteCorMat(8,1:38000);
-        plot(x,right_guess,x,wrong_guess_1,x,wrong_guess_2);
-        ylim([-1,1]);
-    end
 key_final(i) = keyHypo(key);
+
+%Traces first byte
+
+%     if i==1
+%         x=1:38000;
+%         right_guess=ByteCorMat(key,1:38000);
+%         wrong_guess_1=ByteCorMat(9,1:38000);
+%         wrong_guess_2=ByteCorMat(128,1:38000);
+%         plot(x,right_guess,x,wrong_guess_1,x,wrong_guess_2);
+%         ylim([-1,1]);
+%     end
+
+% Find order of the correct key from the best candidate
+
+% ByteCorMatSort=sort(ByteCorMat,2,'descend');
+% [B,I] = sort(ByteCorMatSort(:,1),'descend');
+% order=find(ismember(I,correct_key(i)+1))
 end
 
-for i = 1:16
-    key_hexa=dec2hex(key_final(i))
-end
+% for i = 1:16
+%     key_hexa=dec2hex(key_final(i))
+% end
